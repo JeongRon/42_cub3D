cc313cc4579d420ab2137fb49b928600e9e0226b
이정론 <jeongrol@ijeonglons-MacBook-Air.local>
2023-10-14 22:27:28 +0900

    [fix] 이미지가 없을 시, 에러 처리

--- bonus/cub3d.c
+++ bonus/cub3d.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dongmiki <dongmiki@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jeongrol <jeongrol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 17:22:14 by jeongrol          #+#    #+#             */
-/*   Updated: 2023/10/13 16:20:33 by dongmiki         ###   ########.fr       */
+/*   Updated: 2023/10/14 22:01:52 by jeongrol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,8 +31,6 @@ int	main(int ac, char **av)
 	info.ani = 0;
 	info.ani_time = 0;
 	//excution
-
-	info.map[5][5] = 'D';
 	mlx_hook(info.win, KEY_EVENT_PRESS, 0, &key_press, &info);
 	mlx_hook(info.win, KEY_EVENT_EXIT, 0, &exit_button, &info);
 	mlx_loop_hook(info.mlx, &draw_loop, &info);
--- bonus/excute/excute_setting.c
+++ bonus/excute/excute_setting.c
@@ -6,7 +6,7 @@
 /*   By: jeongrol <jeongrol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 14:58:44 by dongmiki          #+#    #+#             */
-/*   Updated: 2023/10/14 21:02:08 by jeongrol         ###   ########.fr       */
+/*   Updated: 2023/10/14 22:25:47 by jeongrol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,8 @@ static void	setting_mlx_map_tex(t_info *game, t_img *map_tex, int dir)
 		map_tex->img = mlx_xpm_file_to_image(game->mlx, game->ea, \
 		&(map_tex->width), &(map_tex->height));
 	}
+	if (!map_tex->img)
+		ft_error("IMAGE NOT FOUND");
 	map_tex->data = (int *)mlx_get_data_addr(map_tex->img, &(map_tex->bpp), \
 	&(map_tex->line_size), &(map_tex->endian));
 }
@@ -45,6 +47,8 @@ static void	setting_mlx_dir_vec(t_info *game)
 	int	temp;
 
 	temp = -1;
+	game->camera.x = 0;
+	game->camera.y = 0.66;
 	game->dir_vec.x = -1;
 	game->dir_vec.y = 0;
 	rotate(A, game);
@@ -90,10 +94,16 @@ static void	xpm_image(t_info *game)
 {
 	game->sprite.img = mlx_xpm_file_to_image(game->mlx, "./img/crosshair.xpm", \
 		&(game->sprite.width), &(game->sprite.height));
+	if (!game->sprite.img)
+		ft_error("IMAGE NOT FOUND");
 	game->rider.img = mlx_xpm_file_to_image(game->mlx, "./img/rider.xpm", \
 		&(game->rider.width), &(game->rider.height));
+	if (!game->rider.img)
+		ft_error("IMAGE NOT FOUND");
 	game->boom.img = mlx_xpm_file_to_image(game->mlx, "./img/boom.xpm", \
 		&(game->boom.width), &(game->boom.height));
+	if (!game->boom.img)
+		ft_error("IMAGE NOT FOUND");
 }
 
 void	setting_mlx(t_info *game, int i)
@@ -105,20 +115,20 @@ void	setting_mlx(t_info *game, int i)
 	i = -1;
 	while (++i < 4)
 		setting_mlx_map_tex(game, &(game->map_tex[i]), i);
-	i = -1;
 	xpm_image(game);
+	i = -1;
 	while (++i < 13)
 	{
 		str = setting_mlx_image_string(i);
 		game->door[i].img = mlx_xpm_file_to_image(game->mlx, str, \
 		&(game->door[i].width), &(game->door[i].height));
+		if (!game->door[i].img)
+			ft_error("IMAGE NOT FOUND");
 		game->door[i].data = (int *)mlx_get_data_addr(game->door[i].img, \
 		&(game->door[i].bpp), &(game->door[i].line_size), \
 		&(game->door[i].endian));
 		free(str);
 	}
-	game->camera.x = 0;
-	game->camera.y = 0.66;
 	setting_mlx_dir_vec(game);
 	mlx_mouse_hide();
 	game->mouse = 1;
